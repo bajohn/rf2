c49d9437d7795be1882afc9884f9fa6c881e67bb
Brendan John <bjohn454@gmail.com>
2020-11-12 22:30:09 -0500

    multi card moves working

--- src/app/pagecomponents/room/room.component.ts
+++ src/app/pagecomponents/room/room.component.ts
@@ -2,7 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { v4 as uuidv4 } from 'uuid';
 
-import { APIService } from 'src/app/API.service';
+import { APIService, OnUpdateCardSubscription, UpdateCardInput } from 'src/app/API.service';
+
+interface localCard {
+  cardValue: string
+  cardX: number
+  cardY: number
+  cardZ: number
+  cardBeingDragged: boolean,
+  faceUp: boolean
+  lastUpdateTime: number
+  lastOwner: string
+}
 
 @Component({
   selector: 'app-room',
@@ -13,9 +24,12 @@ export class RoomComponent implements OnInit {
 
   public readonly playerId: string;
   public readonly roomId: string;
-  public cardValues = [];
+  public cards: { [key: string]: localCard } = {};
+  public cardValues: string[] = [];
 
-
+  private readonly cardHeight = 105;
+  private readonly cardWidth = 75;
+  private readonly updateMinMs = 100;
   constructor(
     private router: Router,
     private api: APIService
@@ -28,12 +42,37 @@ export class RoomComponent implements OnInit {
   ngOnInit(): void {
 
     this.listCards();
+    this.api.OnUpdateCardListener.subscribe((event: any) => {
+      // Unsure why we have to dig value.data.
+      const cardUpdate: OnUpdateCardSubscription = event.value.data.onUpdateCard;
+      const curCard = this.cards[cardUpdate.cardValue];
+      if (!curCard.cardBeingDragged && cardUpdate.lastOwner !== this.playerId) {
+        curCard.cardX = cardUpdate.x;
+        curCard.cardY = cardUpdate.y;
+        curCard.cardZ = cardUpdate.z;
+      }
+    })
   }
 
   async listCards() {
     const resp = await this.api.ListCards(this.roomId);
     this.cardValues = resp.items.map(el => el.cardValue);
-    //this.cardValues = ['JS']
+
+    for (const card of resp.items) {
+      const cardObjToPush: localCard = {
+        cardValue: card.cardValue,
+        cardX: card.x,
+        cardY: card.y,
+        cardZ: card.z,
+        cardBeingDragged: false,
+        faceUp: card.faceUp,
+        lastUpdateTime: 0,
+        lastOwner: card.lastOwner
+      }
+      this.cards[card.cardValue] = cardObjToPush;
+    }
+    console.log(this.cards);
+
   }
 
   getPlayerId(roomId) {
@@ -47,6 +86,83 @@ export class RoomComponent implements OnInit {
       return curPlayerId;
     }
   }
+
+  mouseMove(event: MouseEvent) {
+    //const curCard = this.cards[cardValue];
+    for (const cardValue of this.cardValues) {
+      const curCard = this.cards[cardValue];
+      if (curCard.cardBeingDragged) {
+        const curTime = (new Date()).getTime();
+        const x = event.x;
+        const y = event.y;
+
+        curCard.cardX = Math.round(x - this.cardWidth / 2);
+        curCard.cardY = Math.round(y - this.cardHeight / 2);
+        if (curTime - curCard.lastUpdateTime > this.updateMinMs) {
+          curCard.lastUpdateTime = curTime;
+          this.publishUpdate(curCard);
+        }
+      }
+    }
+  }
+
+  publishUpdate(curCard: localCard) {
+
+    const cardUpdate: UpdateCardInput = {
+      roomId: this.roomId,
+      cardValue: curCard.cardValue,
+      x: curCard.cardX,
+      y: curCard.cardY,
+      lastOwner: this.playerId
+    }
+    this.api.UpdateCard(cardUpdate);
+
+  }
+
+  getWidth() {
+    return this.cardWidth + 'px';
+  }
+
+  getHeight() {
+    return this.cardHeight + 'px';
+  }
+
+  getFrontImgSrc(cardValue) {
+    return `assets/cards/${cardValue}.svg`;
+  }
+
+  getTransform(cardValue) {
+    const curCard = this.cards[cardValue];
+    return `translate3d(${curCard.cardX}px, ${curCard.cardY}px, 0px)`;
+  }
+
+  mouseUp() {
+    for (const cardValue of this.cardValues) {
+      const curCard = this.cards[cardValue];
+      if (curCard.cardBeingDragged) {
+        this.publishUpdate(curCard);
+        curCard.cardBeingDragged = false;
+      }
+    }
+
+
+  }
+
+  mouseDown(cardValue) {
+    console.log(cardValue);
+    this.cards[cardValue].cardBeingDragged = true;
+
+  }
+
+  cardBeingDragged(cardValue) {
+    return this.cards[cardValue].cardBeingDragged;
+  }
+
+  getZ(cardValue){
+    return this.cards[cardValue].cardZ;
+  }
+
+
 
 
 }
--- src/app/subcomponents/card/card.component.ts
+++ src/app/subcomponents/card/card.component.ts
@@ -15,34 +15,24 @@ export class CardComponent implements OnInit {
   private cardX = 0;
   private cardY = 0;
   public cardZ = 0;
-  private readonly cardHeight = 105;
-  private readonly cardWidth = 75;
+
 
   private lastUpdateTime = 0;
   @Input() playerId: string;
   @Input() roomId: string;
   @Input() cardValue: string;
-  updateMinMs = 100;
 
 
-  constructor(private api: APIService) {
+
+  constructor() {
 
   }
 
   ngOnInit() {
 
-    this.initCard();
+    // this.initCard();
 
-    this.api.OnUpdateCardListener.subscribe((event: any) => {
-      // Unsure why we have to dig value.data.
-      const cardUpdate: OnUpdateCardSubscription = event.value.data.onUpdateCard;
 
-      if (!this.cardBeingDragged && cardUpdate.lastOwner !== this.playerId) {
-        this.cardX = cardUpdate.x;
-        this.cardY = cardUpdate.y;
-        this.cardZ = cardUpdate.z;
-      }
-    })
 
 
 
@@ -51,67 +41,23 @@ export class CardComponent implements OnInit {
 
 
 
-  async initCard() {
-    const card = await this.api.GetCard(this.roomId, this.cardValue);
-    console.log(card);
-    this.cardX = card.x;
-    this.cardY = card.y;
-    this.cardZ = card.z;
-  }
-
-  mouseDown() {
-    this.cardBeingDragged = true;
-  }
-
-  mouseUp() {
-    if (this.cardBeingDragged) {
-      this.publishUpdate();
-    }
-    this.cardBeingDragged = false;
-  }
-
-  mouseMove(event: MouseEvent) {
-    if (this.cardBeingDragged) {
-      const curTime = (new Date()).getTime();
-      const x = event.x;
-      const y = event.y;
-
-      this.cardX = Math.round(x - this.cardWidth / 2);
-      this.cardY = Math.round(y - this.cardHeight / 2);
-      if (curTime - this.lastUpdateTime > this.updateMinMs) {
-        this.lastUpdateTime = curTime;
-        this.publishUpdate();
-      }
-    }
-  }
-
-  getFrontImgSrc() {
-    return `assets/cards/${this.cardValue}.svg`;
-  }
-
-  getTransform() {
-    return `translate3d(${this.cardX}px, ${this.cardY}px, 0px)`;
-  }
+  // async initCard() {
+  //   const card = await this.api.GetCard(this.roomId, this.cardValue);
+  //   console.log(card);
+  //   this.cardX = card.x;
+  //   this.cardY = card.y;
+  //   this.cardZ = card.z;
+  // }
 
 
-  publishUpdate() {
-    const cardUpdate: UpdateCardInput = {
-      roomId: this.roomId,
-      cardValue: this.cardValue,
-      x: this.cardX,
-      y: this.cardY,
-      lastOwner: this.playerId
-    }
-    this.api.UpdateCard(cardUpdate);
 
-  }
 
-  getWidth() {
-    return this.cardWidth + 'px';
-  }
 
-  getHeight() {
-    return this.cardHeight + 'px';
-  }
+  
+
+
+
+
+
 
 }
