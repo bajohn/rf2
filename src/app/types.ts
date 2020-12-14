0be69675dc3671d616a7fb92d6a65a1963e3cf0b
Brendan John <bjohn454@gmail.com>
2020-12-14 15:16:36 -0800

    more planning

--- src/app/types.ts
+++ src/app/types.ts
@@ -3,22 +3,14 @@
 // currently used for planning, will probably throw this out later
 
 type room = {
-    roomId: string 
+    roomId: string  // key 
     players: player[]
-    gameState: gameState 
+    // gameState: gameState  // RF only
     cards: card[]
+    cardStacks: cardStack[]
 }
 
-type gameState = {
-    turn: number 
-    bids: bid[]
-}
 
-type bid = {
-    playerId: string 
-    bid: number 
-    actual: number // start at -1, fill in with actual value on turn completion
-}
 
 type moveable = {
     id: string 
@@ -31,14 +23,43 @@ type moveable = {
     inMotion: boolean 
 }
 
+// moveable
 type player = {
-    id: string
+    id: string //key 
+    roomId: string // secondary index
     name: string 
     position: number 
 }
 
-
+// moveable
 type card = {
+    id: string 
     faceUp: boolean
     cardValue: string 
+}
+
+// moveable
+type cardStack = {
+ id: string 
+ spread: boolean // each card offset or not 
+ cards: card[] 
+
+}
+
+//////////////////////////////
+// RF only:
+////////////
+
+type gameState = {
+    roomId: string // key
+    turn: number 
+    bids: bid[]
+}
+
+// raw type, not a model subscribed to
+type bid = {
+    turn: number 
+    playerId: string 
+    bid: number 
+    actual: number // start at -1, fill in with actual value on turn completion
 }
